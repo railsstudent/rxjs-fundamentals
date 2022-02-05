c3cb395d3d0625e939cdc70a778196cc568fdd9e
Connie Leung <cleung334@gmail.com>
2022-02-05 17:33:59 +0800

    feat: redo basic counter ui with skipUntil and takeUntil

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,18 +1,27 @@
 import { fromEvent, interval, merge, NEVER } from 'rxjs';
+import { skipUntil, takeUntil, scan } from 'rxjs/operators'
 import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 
 const interval$ = interval(1000)
-let subscription
+  .pipe(
+    skipUntil(start$),
+    scan(total => total + 1, 0),
+    takeUntil(pause$),
+  )
 
-start$.subscribe(() => {
-  subscription = interval$.subscribe(setCount)
-})
+interval$.subscribe(setCount)
 
-pause$.subscribe(() => { 
-  if (subscription) {
-    subscription.unsubscribe()
-  }
-})
+// let subscription
+
+// start$.subscribe(() => {
+//   subscription = interval$.subscribe(setCount)
+// })
+
+// pause$.subscribe(() => { 
+//   if (subscription) {
+//     subscription.unsubscribe()
+//   }
+// })
