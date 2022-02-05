65033a1172780f873678471d96e398edc1756326
Connie Leung <cleung334@gmail.com>
2022-02-05 13:53:18 +0800

    feat: complete basic counter course

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -3,3 +3,16 @@ import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+const interval$ = interval(1000)
+let subscription
+
+start$.subscribe(() => {
+  subscription = interval$.subscribe(setCount)
+})
+
+pause$.subscribe(() => { 
+  if (subscription) {
+    subscription.unsubscribe()
+  }
+})
