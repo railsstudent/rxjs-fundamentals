4d0968f6addab68ff38fe812dd4ea711d3a0713f
Connie Leung <cleung334@gmail.com>
2022-02-05 21:49:35 +0800

    feat: use switchMap to create counter

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,18 +1,18 @@
 import { fromEvent, interval, merge, NEVER } from 'rxjs';
-import { skipUntil, takeUntil, scan } from 'rxjs/operators'
+import { skipUntil, takeUntil, scan, mapTo, startWith, switchMap } from 'rxjs/operators'
 import { setCount, startButton, pauseButton } from './utilities';
 
-const start$ = fromEvent(startButton, 'click');
-const pause$ = fromEvent(pauseButton, 'click');
+// const start$ = fromEvent(startButton, 'click');
+// const pause$ = fromEvent(pauseButton, 'click');
 
-const interval$ = interval(1000)
-  .pipe(
-    skipUntil(start$),
-    scan(total => total + 1, 0),
-    takeUntil(pause$),
-  )
+// const interval$ = interval(1000)
+//   .pipe(
+//     skipUntil(start$),
+//     scan(total => total + 1, 0),
+//     takeUntil(pause$),
+//   )
 
-interval$.subscribe(setCount)
+// interval$.subscribe(setCount)
 
 // let subscription
 
@@ -25,3 +25,17 @@ interval$.subscribe(setCount)
 //     subscription.unsubscribe()
 //   }
 // })
+
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+const isRunning$ = merge(start$, pause$).pipe(startWith(false))
+
+const counter$ = isRunning$
+  .pipe(
+    switchMap((isRunning) => isRunning ? interval(1000) : NEVER),
+    //skipUntil(start$),
+    scan(total => total + 1, 0),
+    //takeUntil(pause$),
+  )
+
+counter$.subscribe(setCount)
