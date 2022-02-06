a948d82c10dd477692480cc68cef26d7bfed9e36
Connie Leung <cleung334@gmail.com>
2022-02-06 21:19:39 +0800

    feat:  retrieve Pokemon details

--- applications/pokemon/script.js
+++ applications/pokemon/script.js
@@ -15,6 +15,7 @@ import {
   take,
   EMPTY,
   pluck,
+  retry,
 } from 'rxjs';
 
 import { fromFetch } from 'rxjs/fetch';
@@ -26,11 +27,12 @@ import {
   endpointFor,
   search,
   form,
+  renderPokemon
 } from '../pokemon/utilities';
 
 const endpoint = 'http://localhost:3333/api/pokemon/';
 
-const search$ = fromEvent(search, 'input')
+const oldSearch$ = fromEvent(search, 'input')
   .pipe(
     debounceTime(300),
     map(event => event.target.value),
@@ -55,6 +57,62 @@ const search$ = fromEvent(search, 'input')
     tap(clearResults),
     pluck('pokemon'),
     tap(addResults),
+  )
+
+const searchPokemons = (searchTerm) => 
+  fromFetch(endpoint + 'search/' + searchTerm + '?delay=100&chaos=true&flakiness=2')
+    .pipe(
+      mergeMap((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw new Error('Something went wrong!')
+      }),
+      retry(4),
+      catchError(err => {
+        console.warn(err)
+        return of({
+          pokemon: []
+        })
+      })
+    )
+
+const getPokemonDetails = (id) => 
+  fromFetch(endpoint + id)
+    .pipe(
+      mergeMap((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw new Error('Something went wrong!')
+      }),
+      retry(4),
+      catchError(err => {
+        console.warn(err)
+        return of({
+          pokemon: []
+        })
+      })
+    )
+
+const search$ = fromEvent(form, 'submit')
+  .pipe(
+    map(() => search.value),
+    switchMap(searchPokemons),
+    pluck('pokemon'),
+    mergeMap(pokemons => pokemons),
+    take(1),
+    switchMap(pokemon => {
+      const pokemon$ = of(pokemon)
+      const data$ = getPokemonDetails(pokemon.id)
+        .pipe(map(data => ({
+            ...pokemon, 
+            data
+          })
+        ))
+      return merge(pokemon$, data$)
+    }),
+    tap(renderPokemon)
   )
 
 search$.subscribe(console.log)
