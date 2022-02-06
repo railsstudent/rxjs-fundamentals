0962086c0d58388a6fbbc67e2da87baf2f372af8
Connie Leung <cleung334@gmail.com>
2022-02-06 13:58:54 +0800

    feat:  fetch dog facts every 5 seconds

--- applications/dog-facts/script.js
+++ applications/dog-facts/script.js
@@ -23,34 +23,50 @@ import {
 
 const endpoint = 'http://localhost:3333/api/facts?count=3&delay=1000&chaos=true&flakiness=2';
 
-const fetchFacts$ = fromEvent(fetchButton, 'click')
+const fetchData = () => {
+  return fromFetch(endpoint)
+    .pipe(
+      mergeMap(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw new Error(response.statusText);
+      }),
+      retry(4),
+      catchError((err) => {
+        console.warn(err)
+        return of({ error: err.message })
+      })
+    )
+}
+
+const fetch$ = fromEvent(fetchButton, 'click').pipe(mapTo(true))
+const stop$ = fromEvent(stopButton, 'click').pipe(mapTo(false))
+
+const loadDogFacts = (shouldFetch) => {
+  if (shouldFetch) {
+    // fetch data immedialy and load again every 5 seconds
+    return timer(0, 5000)
+      .pipe(
+        tap(clearError),
+        tap(clearFacts),
+        exhaustMap(fetchData)
+      )
+  }
+  return NEVER
+}
+
+const fetchFacts$ = merge(fetch$, stop$)
   .pipe(
-    exhaustMap(() => {
-      return fromFetch(endpoint)
-        .pipe(
-          mergeMap(response => {
-            if (response.ok) {
-              return response.json()
-            }
-            throw new Error(response.statusText);
-          }),
-          retry(4),
-          catchError((err) => {
-            console.warn(err)
-            return of({ error: err.message })
-          })
-        )
-    }),
+    startWith(false),
+    switchMap(loadDogFacts),
     tap(console.log)
   )
-
 
 fetchFacts$.subscribe(({ error, facts }) => {
   if (error) {
     return setError(error)
   }
 
-  clearError()
-  clearFacts()
   return addFacts({ facts })
 })
